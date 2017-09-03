0b4e287e9a915afc53155b60b46f847c69beaaa4
佐藤秀輔 <sato.fun0619@gmail.com>
2017-09-03 13:25:49 +0900

    [add] add unityproject

---
+++ PlayerTest/Assets/Script/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (-0.1f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate ( 0.1f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate ( 0, 0.1f, 0);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate ( 0, -0.1f, 0);
+		}
+	}
+}
